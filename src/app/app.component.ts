b5a16a1c0ef71e9daf60682b9f53e3c5102d7733
RafiTahmid <rafi.tahmid019@gmail.com>
2023-03-27 00:01:12 +0600

    added bs,js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-todo-app';
+  constructor() {
+    setTimeout(() => {
+      this.title = "Changed title";
+    }, 2000)
+  }
 }
